0811bc88c9d98d69776a2a95a8d30f19e6f8611d
MattiaVolonterio <volonteriomattia@gmail.com>
2024-04-12 16:41:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title: "Boolfolio",
+    };
+  },
+  props: {},
+  methods: {},
+
+  created() {
+    console.log(axios);
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+@use "/src/scss/general.scss";
+</style>
