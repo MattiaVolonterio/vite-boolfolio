b19a83bb0b1c264b4e7ecc4f0b44a07f1db73886
MattiaVolonterio <volonteriomattia@gmail.com>
2024-04-12 17:52:03 +0200

    feat: pagination of projects

--- src/components/ProjectsMain.vue
+++ src/components/ProjectsMain.vue
@@ -9,22 +9,44 @@ export default {
     return { store };
   },
   props: {},
-  methods: {},
+  methods: {
+    fetchProject(endpoint = api.baseUrl + "projects") {
+      axios.get(endpoint).then(function (response) {
+        store.projects = response.data.data;
+        store.projectsLinks = response.data.links;
+      });
+    },
+  },
 
   components: { ProjectCard },
 
   created() {
-    axios.get(api.baseUrl + "projects").then(function (response) {
-      store.projects = response.data.data;
-    });
+    this.fetchProject();
   },
 };
 </script>
 
 <template>
-  <div class="row row-cols-2 g-3">
+  <div class="row row-cols-2 g-3 mb-5">
     <project-card v-for="project in store.projects" :project="project" />
   </div>
+
+  <nav aria-label="Page navigation">
+    <ul class="pagination">
+      <li class="page-item" v-for="link in store.projectsLinks">
+        <a
+          :class="{
+            active: link.active,
+            disabled: !link.url,
+          }"
+          class="page-link"
+          href="#"
+          v-html="link.label"
+          @click="fetchProject(link.url)"
+        ></a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss" scoped></style>
